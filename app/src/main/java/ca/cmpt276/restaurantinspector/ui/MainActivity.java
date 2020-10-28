76403f58ad33be2514858bd54f14685e29c2c5d6
mdzheng <mdzheng@sfu.ca>
2020-10-28 12:37:38 -0700

    Initial Commit

---
+++ app/src/main/java/ca/cmpt276/restaurantinspector/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package ca.cmpt276.restaurantinspector.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.restaurantinspector.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
